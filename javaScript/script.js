53278b8dd638c89dedf0313208b1a25f9a3dfb39
Willian dos Santos Silva <86166759+Wiillboss@users.noreply.github.com>
2026-01-30 23:44:38 -0300

    aula sobre API
    Explicações e anotações feitas, apenas.

---
+++ javaScript/script.js
@@ -0,0 +1,7 @@
+/*
+Como uma API funciona
+API = Aplication Programming Interface
+
+Padrão de comunicação JSON é um dos mais usados para interface entre API.
+JSON = Javascript Object Notation - Notação de objeto javascript
+*/ 
